99575468b3f8c12e1a7ccb8e7c03745c1c9890c9
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-27 17:50:31 -0500

    feat: :construction: Manejo de metodos CRUD: /Acudiente
    Se crea el endpoint mencionado en este commit, para manejar los metodos crud y tener control de la data de la base de datos de "Acudiente" por medio de sus diferentes rutas.

---
+++ backend/routes/Cacudiente.js
@@ -0,0 +1,69 @@
+import { Router } from "express";
+import {limitGColecciones, limitPColecciones, limitDColecciones} from '../middleware/limit.js';
+import errorcontroller from "../middleware/erroresMongo.js";
+import { con } from '../db/atlas.js';
+
+const AppAcudiente = Router();
+let db = await con();
+let acudiente = db.collection("acudiente");
+let autoincrement = db.collection("autoincrement");
+async function  increment(coleccion){
+    const sequenceDocument = await autoincrement.findOneAndUpdate(
+        { _id: `${coleccion}Id` },
+        { $inc: { sequence_value: 1 } },
+        { returnDocument: "after" }
+    );
+    return sequenceDocument.value.sequence_value;
+}
+AppAcudiente.get('/GetAcudiente', limitGColecciones(), async (req, res) =>{
+    if(!req.rateLimit) return;
+    let result = await acudiente.find({}).sort( { _id: 1 } ).toArray();
+    res.send(result)
+
+})
+
+AppAcudiente.post('/PostAcudiente', limitPColecciones(220, "Acudiente"), async (req, res) =>{
+    if(!req.rateLimit) return;
+    const id =  await increment("acudiente");
+    let data = {_id: id, ...req.body}
+    try { 
+        let result = await acudiente.insertOne(data)
+        res.status(200).send({status: 200, message: `Documento con el id ${id} se ha creado Correctamente`});
+      } catch (error) {
+        errorcontroller(error, res);
+      }
+})
+
+AppAcudiente.put('/PutAcudiente', limitPColecciones(220, "Acudiente"), async (req, res) =>{
+    if(!req.rateLimit) return;
+    const id = parseInt(req.query.id, 10);
+    let data = {...req.body}
+    try {
+        
+        let result = await acudiente.updateOne({ _id: id }, { $set: data })
+        if (result.modifiedCount > 0) {
+            res.status(200).send({status: 200, message: `Documento con el id ${id} se ha actualizado Correctamente`});
+        } else {
+            result.matchedCount === 1
+            ? res.status(200).send({ status: 200, message:`No se realizaron cambios en el documento con el id ${id}`})
+            : res.status(404).send({ status: 404, message:`El documento con el id ${id} no ha sido encontrado`});
+        }
+      } catch (error) {
+        errorcontroller(error, res);
+      }
+})
+
+AppAcudiente.delete('/DeleteAcudiente', limitDColecciones(), async (req, res) =>{
+    if(!req.rateLimit) return;
+    const id = parseInt(req.body.id, 10);
+    try {
+        let result = await acudiente.deleteOne({ _id: id })
+        result.deletedCount === 1
+        ? res.status(200).send({ status: 200, message:`Documento con el id ${id} ha sido eliminado correctamente`})
+        : res.status(404).send({ status: 404, message:`El documento con el id ${id} no ha sido encontrado`});
+      } catch (error) {
+        errorcontroller(error, res);
+      }
+})
+
+export default AppAcudiente;
