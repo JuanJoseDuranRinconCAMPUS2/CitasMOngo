3598f8f00d82d5a3a815e7c430e2de9102ac7707
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-27 16:17:39 -0500

    feat: :sparkles: Implementacion Endpoin: /Cita
    se implementa el endpoint "Cita" en la api principal de la base de datos para el manejo de las citas

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import AppEspecialidad from "./routes/Cespecialidad.js";
 import AppConsultorio from "./routes/Cconsultorio.js";
 import AppMedico from "./routes/Cmedico.js";
+import AppCita from "./routes/Ccita.js";
 
 console.clear();
 dotenv.config("../");
@@ -16,7 +17,7 @@ ApiCitas.use(express.json());
 ApiCitas.use('/Especialidad', AppEspecialidad);
 ApiCitas.use('/Consultorio', AppConsultorio);
 ApiCitas.use('/Medico', AppMedico);
-
+ApiCitas.use('/Cita', AppCita);
 // ════════ ⋆★⋆ ════════
 
 //Rutas de validacion
