28c2b7b32aee2d8dc337dc5e8b336dded91484e0
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-27 18:44:05 -0500

    feat: :construction: Manejo de metodos CRUD: /Genero
    Se crea el endpoint mencionado en este commit, para manejar los metodos crud y tener control de la data de la base de datos de "Genero" por medio de sus diferentes rutas.

---
+++ backend/routes/Cgenero.js
@@ -0,0 +1,70 @@
+import { Router } from "express";
+import {limitGColecciones, limitPColecciones, limitDColecciones} from '../middleware/limit.js';
+import errorcontroller from "../middleware/erroresMongo.js";
+import { con } from '../db/atlas.js';
+
+const AppGenero = Router();
+let db = await con();
+let genero = db.collection("genero");
+let autoincrement = db.collection("autoincrement");
+async function  increment(coleccion){
+    const sequenceDocument = await autoincrement.findOneAndUpdate(
+        { _id: `${coleccion}Id` },
+        { $inc: { sequence_value: 1 } },
+        { returnDocument: "after" }
+    );
+    return sequenceDocument.value.sequence_value;
+}
+AppGenero.get('/GetGenero', limitGColecciones(), async (req, res) =>{
+    if(!req.rateLimit) return;
+    let result = await genero.find({}).sort( { _id: 1 } ).toArray();
+    res.send(result)
+
+})
+
+AppGenero.post('/PostGenero', limitPColecciones(65, "Genero"), async (req, res) =>{
+    if(!req.rateLimit) return;
+    const id =  await increment("genero");
+    let data = {_id: id, ...req.body}
+    try { 
+        let result = await genero.insertOne(data)
+        console.log(result);
+        res.status(200).send({status: 200, message: `Documento con el id ${id} se ha creado Correctamente`});
+      } catch (error) {
+        errorcontroller(error, res);
+      }
+})
+
+AppGenero.put('/PutGenero', limitPColecciones(65, "Genero"), async (req, res) =>{
+    if(!req.rateLimit) return;
+    const id = parseInt(req.query.id, 10);
+    let data = {...req.body}
+    try {
+        
+        let result = await genero.updateOne({ _id: id }, { $set: data })
+        if (result.modifiedCount > 0) {
+            res.status(200).send({status: 200, message: `Documento con el id ${id} se ha actualizado Correctamente`});
+        } else {
+            result.matchedCount === 1
+            ? res.status(200).send({ status: 200, message:`No se realizaron cambios en el documento con el id ${id}`})
+            : res.status(404).send({ status: 404, message:`El documento con el id ${id} no ha sido encontrado`});
+        }
+      } catch (error) {
+        errorcontroller(error, res);
+      }
+})
+
+AppGenero.delete('/DeleteGenero', limitDColecciones(), async (req, res) =>{
+    if(!req.rateLimit) return;
+    const id = parseInt(req.body.id, 10);
+    try {
+        let result = await genero.deleteOne({ _id: id })
+        result.deletedCount === 1
+        ? res.status(200).send({ status: 200, message:`Documento con el id ${id} ha sido eliminado correctamente`})
+        : res.status(404).send({ status: 404, message:`El documento con el id ${id} no ha sido encontrado`});
+      } catch (error) {
+        errorcontroller(error, res);
+      }
+})
+
+export default AppGenero;
